b0ce2b58782df2415d5ec6c1f3f38ae5102aec89
babafemiolatona <femiolatona@gmail.com>
2026-03-04 14:24:51 +0100

    chore: configure postgres, JPA and docker setup

--- src/main/java/com/nanolink/NanolinkApplication.java
+++ src/main/java/com/nanolink/NanolinkApplication.java
@@ -2,12 +2,20 @@ package com.nanolink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NanolinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NanolinkApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, World!";
 	}
 
 }
