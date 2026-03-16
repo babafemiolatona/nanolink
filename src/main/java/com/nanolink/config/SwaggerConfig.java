9e3dd0752e7390e04c15e2407b472eb6283e8f05
babafemiolatona <femiolatona@gmail.com>
2026-03-16 15:35:19 +0100

    feat: include security scheme for JWT authentication

--- src/main/java/com/nanolink/config/SwaggerConfig.java
+++ src/main/java/com/nanolink/config/SwaggerConfig.java
@@ -3,15 +3,27 @@ package com.nanolink.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_SCHEME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+        .components(new Components().addSecuritySchemes(BEARER_SCHEME,
+            new SecurityScheme()
+                .name(BEARER_SCHEME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Nanolink API")
                         .version("1.0")
